fc16c29d51ee20614c39717efc208e073e8abfea
Kelvin Wong <kc2wong@hotmail.com>
2024-12-22 09:29:28 +0000

    reuse existing feature in <Transition>
    use onExited to trigger the callback

--- src/contexts/PageTransition.tsx
+++ src/contexts/PageTransition.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useState, useRef } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useRef,
+  useCallback,
+} from 'react';
 import { Transition, TransitionStatus } from 'react-transition-group';
 
 type TransitionStyles = {
@@ -24,31 +30,42 @@ const transitionStyles: TransitionStyles = {
 };
 
 export const PageTransitionProvider: React.FC<{
-  duration?:number,
+  duration?: number;
   children: React.ReactNode;
 }> = (props) => {
   const [show, setShow] = useState(true);
   const nodeRef = useRef<HTMLDivElement | null>(null);
+  const callbackRef = useRef<(() => void) | null>(null);
 
   const children = props.children;
   const duration = props.duration ?? defaultDuration;
-  
+
   const defaultStyle: React.CSSProperties = {
     transition: `opacity ${duration}ms ease-in-out`,
     opacity: 0,
   };
 
-  const startTransition = (callback: () => void) => {
+  const startTransition = useCallback((callback: () => void) => {
+    callbackRef.current = callback; // Store the callback
     setShow(false); // Start fade-out
-    setTimeout(() => {
-      callback(); // Perform callback (e.g., change page)
-      setShow(true); // Start fade-in
-    }, duration);
+  }, []);
+
+  const handleExited = () => {
+    if (callbackRef.current) {
+      callbackRef.current(); // Execute the callback
+      callbackRef.current = null; // Reset the callback
+    }
+    setShow(true); // Start fade-in
   };
 
   return (
     <PageTransitionContext.Provider value={{ startTransition }}>
-      <Transition nodeRef={nodeRef} in={show} timeout={duration}>
+      <Transition
+        nodeRef={nodeRef}
+        in={show}
+        timeout={duration}
+        onExited={handleExited}
+      >
         {(state) => (
           <div
             ref={nodeRef}
